f478db7e6302e295546c75366b7a7f0dac42a889
Ian Samuel Valdovinos Granados <ian.valdovinos752001@gmail.com>
2025-06-11 23:53:00 -0400

    Add basic CRUD Project endpoints

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 from starlette import status
 from typing import Annotated
 
@@ -20,6 +21,19 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class ProjectRequest(BaseModel):
+    title: str = Field(min_length=1, max_length=30)
+    description: str= Field(min_length=1, max_length=200)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "PROJECT_TITLE",
+                "description": "PROJECT_DESCRIPTION"
+            }
+        }
+    }
+
 @app.get("/author", status_code=status.HTTP_200_OK)
 async def get_author():
     return {"author": "Ian Samuel Valdovinos Granados"}
@@ -35,3 +49,31 @@ async def get_project_by_id(db: db_dependency, project_id: int = Path(gt=0)):
         return todo_model
 
     raise HTTPException(status_code=404, detail="Project not found.")
+
+@app.post("/project", status_code=status.HTTP_201_CREATED)
+async def create_project(db: db_dependency, project_request: ProjectRequest):
+    project_request = Projects(**project_request.model_dump())
+
+    db.add(project_request)
+    db.commit()
+
+@app.put("/project/{project_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_project(db: db_dependency, project_request: ProjectRequest, project_id: int = Path(gt=0)):
+    project_model = db.query(Projects).filter(Projects.id == project_id).first()
+    if project_model is None:
+        raise HTTPException(status_code=404, detail="Project not found.")
+
+    project_model.title = project_request.title
+    project_model.description = project_request.description
+
+    db.add(project_model)
+    db.commit()
+
+@app.delete("/project/{project_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_project(db: db_dependency, project_id: int = Path(gt=0)):
+    project_model = db.query(Projects).filter(Projects.id == project_id).first()
+    if project_model is None:
+        raise HTTPException(status_code=404, detail="Project not found.")
+
+    db.delete(project_model)
+    db.commit()
